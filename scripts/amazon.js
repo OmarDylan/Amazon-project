a229fb7c7d50ddb1f2719aa669be66278abeb8c4
odabou <odabou@nscu.edu>
2025-05-23 19:43:10 -0400

    Used products.js file to create the products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,38 +1,7 @@
-const products = [
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090
-    },
-    {
-        image: "images/products/intermediate-composite-basketball.jpg",
-        name: "Intermediate Composite Size Basketball",
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        priceCents: 2095
-    },
-    {
-        image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        mame: "Adults Plain Cotton T-Shirt - 2 Pack",
-        rating: {
-            stars:4.5,
-            count: 87
-        },
-        priceCents: 799
-    }
-
-];
 
 let productHTML = "";
-products.forEach(product => {
-
-    productHTML += `<div class="product-container">
+products.forEach((product) => {
+  productHTML += `<div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
@@ -81,7 +50,6 @@ products.forEach(product => {
           </button>
         </div>
 `;
-
 });
 
 console.log(productHTML);

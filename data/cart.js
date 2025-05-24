23d8a459b89fb761af0ce080f55b8b640450809a
odabou <odabou@nscu.edu>
2025-05-24 00:43:21 -0400

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,32 +1,50 @@
-export const cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
+export let cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+    },
 ];
 
 export function addToCart(productId) {
-  let matchingItem;
-  const cartSelectValue = document.querySelector(
-    `.js-product-quantity-${productId}`
-  ).value;
+    let matchingItem;
+    const cartSelectValue = document.querySelector(
+        `.js-product-quantity-${productId}`
+    ).value;
 
-  cart.forEach((cartItem) => {
-    if (cartItem.productId === productId) {
-      matchingItem = cartItem;
-    }
-  });
-
-  if (matchingItem) {
-    matchingItem.quantity += Number(cartSelectValue);
-  } else {
-    cart.push({
-      productId: productId,
-      quantity: Number(cartSelectValue),
+    cart.forEach((cartItem) => {
+        if (cartItem.productId === productId) {
+            matchingItem = cartItem;
+        }
     });
-  }
+
+    if (matchingItem) {
+        matchingItem.quantity += Number(cartSelectValue);
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: Number(cartSelectValue),
+        });
+    }
+}
+
+
+
+export function removeFromCart(productId) {
+
+    const newCart = [];
+
+    cart.forEach(cartItem => {
+
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+
+
+
+    });
+
+    cart = newCart;
 }

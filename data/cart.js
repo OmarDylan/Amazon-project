1e9d93a819c50e3bdf5363683d1dcfd820093d22
odabou <odabou@nscu.edu>
2025-05-24 00:14:38 -0400

    Generate html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,20 @@
-export const cart = [];
-
+export const cart = [
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
 export function addToCart(productId) {
   let matchingItem;
+  const cartSelectValue = document.querySelector(
+    `.js-product-quantity-${productId}`
+  ).value;
+
   cart.forEach((cartItem) => {
     if (cartItem.productId === productId) {
       matchingItem = cartItem;
@@ -10,13 +22,11 @@ export function addToCart(productId) {
   });
 
   if (matchingItem) {
-    matchingItem.quantity++;
+    matchingItem.quantity += Number(cartSelectValue);
   } else {
     cart.push({
       productId: productId,
-      quantity: 1,
+      quantity: Number(cartSelectValue),
     });
   }
 }
-
- 

66daba4d6f016d407f1d6c251391974eeb395e73
odabou <odabou@nscu.edu>
2025-05-24 00:30:20 -0400

    Fix price formatting and radio selected

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,9 +1,10 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let productHTML = "";
 products.forEach((product) => {
-  productHTML += `<div class="product-container">
+    productHTML += `<div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
@@ -22,7 +23,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
@@ -61,19 +62,19 @@ document.querySelector(".js-products-grid").innerHTML = productHTML;
 
 // Update the cart quantity in the header
 function updateCartQuantity() {
-  let cartQuantity = 0;
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
 
-  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 }
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
-    addToCart(productId);
-    updateCartQuantity();
-    
-  });
+    button.addEventListener("click", () => {
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
+
+    });
 });

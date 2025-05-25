fa1944ab5c4d3779b53ba32b19bbb3bf3c6d3c2c
odabou <odabou@nscu.edu>
2025-05-24 23:14:05 -0400

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
@@ -28,7 +28,6 @@ cart.forEach((cartItem) => {
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDate, "day");
     const dateString = deliveryDate.format("dddd, MMMM D");
-
 
     cartSummaryHTML += ` 
 <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -91,9 +90,15 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
         html += ` 
-        <div class = "delivery-option" >
+        <div class = "delivery-option js-delivery-option" data-product-id="${matchingProduct.id}"  data-delivery-option-id="${
+          deliveryOption.id
+        }">
             <input type = "radio"
-            ${isChecked ? "checked": ""} class "delivery-option-input" name = "delivery-option-${matchingProduct.id}" >
+            ${
+              isChecked ? "checked" : ""
+            } class "delivery-option-input" name = "delivery-option-${
+      matchingProduct.id
+    }" >
                 <div>
                     <div class = "delivery-option-date" >
                         ${dateString}
@@ -131,6 +136,21 @@ cart.forEach((cartItem) => {
 document.querySelector(
     ".js-checkout-quantity"
 ).innerHTML = `Checkout (${cartQuantity})`;
+
+
+
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+
+    element.addEventListener("click", () => {
+
+        const { productId, deliveryOptionId } = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
+
+
+    });
+
+});
+
 
 // let updateHTML = `
 // <input type="text"class="quantity-input">
